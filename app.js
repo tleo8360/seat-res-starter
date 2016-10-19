d5cd1a9c094cf96a2841f00ce2a15d7367f3c5c6
Travis <Travis@Traviss-MacBook-Pro.local>
2016-10-18 21:31:20 -0400

    hide form and showed it on click

--- app.js
+++ app.js
@@ -1,8 +1,18 @@
+
+//
+// $(".available").on("click", function(){
+//   var result = window.prompt("Enter name")
+//   $(this).console.log("result");
+// });
 $(".available").on("click", function(){
   $(this).addClass("reserved");
 });
 
+$("form").hide();
 $(".available").on("click", function(){
-  var result = window.prompt("Enter name")
-  $(this).console.log("result");
+  $("form").show();
+});
+
+$(".submit").on("click", function(){
+  $(this).hide();
 });
