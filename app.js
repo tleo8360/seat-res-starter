22a118b73ae15bb335153265b5c3a143b0a008bc
Travis <Travis@Traviss-MacBook-Pro.local>
2016-10-18 20:54:57 -0400

    added click function to prompt window and change div class to reserved

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
-console.log("Hello, World!");
+$(".available").on("click", function(){
+  $(this).addClass("reserved");
+});
+
+$(".available").on("click", function(){
+  var result = window.prompt("Enter name")
+  $(this).console.log("result");
+});
