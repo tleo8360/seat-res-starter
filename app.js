5641f90baccdfecd4cb169250b4f595d1deaa95a
Travis <Travis@Traviss-MacBook-Pro.local>
2016-10-22 10:25:14 -0400

    added form and added jquery functionality

--- app.js
+++ app.js
@@ -1,8 +1,22 @@
 for (var i = 0; i < 24; i ++) {
 var newSeat = $("<div class='available'></div>");
+  newSeat.attr("id", i);
 $(".seat-chart").append(newSeat);
 }
 
-$(".available, .reserved").on("click", function(){
-  $(this).toogleClass("reserved available");
+var currentSeat;
+
+$(".available").on("click", function(){
+  $("form").show();
+  currentSeat = $(this).attr('id');
+  console.log(currentSeat);
+});
+
+$("button").on("click", function(){
+  var userData = {
+    name: "",
+    email: ""
+  };
+  $("form").hide();
+  $("#" + currentSeat).toogleClass("reserved available");
 });
